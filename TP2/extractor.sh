447cec4fa48be18ba86224a20b395a929da694e9
basile fetet <basile.fetet@gmail.com>
2016-03-16 15:40:12 -0400

    mise a jour scripts

--- TP2/extractor.sh
+++ TP2/extractor.sh
@@ -2,13 +2,15 @@
 /home/stev/tp2-app.sh -h | sed -e '/^$/d' > file.txt
 
 # on recupere tous les arguments (ligne commence par  -)
-sed '/^ -/!d' file.txt > arg.txt
+sed '/^ *-/!d' file.txt > arg.txt
 # on recupere toutes les contraintes (contiennent un &)
 sed '/&/!d' file.txt > contraintes.txt
+
 
 # on efface les &, = dans contraintes.txt et les virgules dans arg.txt
 sed -i "s/& //g" contraintes.txt
 sed -i "s/\= //g" contraintes.txt
+sed -i "s/^ *-//g" contraintes.txt
 sed -i "s/,//g" arg.txt
 
 # on appelle le fichier python
--- TP2/question1.py
+++ TP2/question1.py
@@ -1,16 +1,85 @@
+#from enum import Enum
+
 print 'Test'
 f = open('arg.txt','r')
 lignes  = f.readlines()
 f.close()
-liste={}
 
- 
+f = open('contraintes.txt','r')
+constraints = f.readlines()
+f.close
+
+liste={}
+listeArg=[]
+listeContraintes=[]
+
+#Etype = Enum('flag', 'nonFlag')
+
+class Argument:
+
+	def __init__(self,nom,isFlag,values):
+		# contructeur
+		self.nom=nom
+		self.isFlag=isFlag
+		if (isFlag):
+			values=['_on','_off']
+		self.values=values
+	
+	def toString(self):
+		print "nom :"
+		print self.nom
+		print "is flag :"
+		print self.isFlag
+		print "values :"
+		print self.values
+"""
 for ligne in lignes:
 	print ligne
 	
 	values = ligne.split()
 	key=values[0]
 	values.remove(key)
+	if (values == ['flag']):
+		values = ['_on','_off']
 	liste[key] = values
 
 print liste
+"""
+print "test avec classe argument"
+for ligne in lignes:
+	values = ligne.split()
+	nom = values[0]
+	values.remove(nom)	
+	listeArg.append(Argument(nom,(values==['flag']),values))
+
+for element in listeArg :
+	element.toString()
+
+
+for constraint in constraints :
+	values = constraint.split()
+	isFlag=False
+	print values
+	arg1=values[0]
+	for e in listeArg :
+		isFlag=((('-'+arg1)==e.nom) and e.isFlag)
+	if isFlag :
+		val1='_on'
+		arg2=values[1]
+		for e in listeArg :
+			if((('-'+arg2)==e.nom) and e.isFlag):
+				val2='_on'
+			else :
+				val2=values[2]
+	else :
+		val1=values[1]
+		arg2=values[2]
+		for e in listeArg:
+			if(('-'+arg2)==e.nom):
+				if (e.isFlag):
+					val2='_on'
+				else :
+					val2=values[3]
+	listeContraintes.append([arg1,val1,arg2,val2])
+
+print listeContraintes
