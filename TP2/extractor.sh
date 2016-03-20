7d731e4caba63e7cbdab7cde2bf7652cfb62442c
Sovhan <olivier.soldano@e.ujf-grenoble.fr>
2016-03-20 17:14:23 -0400

    scripts v.0a for testing

--- TP2/extractor.sh
+++ TP2/extractor.sh
@@ -1,9 +1,12 @@
 # on recupere la sortie du -h dans file.txt
+echo "reading application help"
 $1 -h | sed -e '/^$/d' > file.txt
 
 # on recupere tous les arguments (ligne commence par  -)
+echo "arguments extraction"
 sed '/^ *-/!d' file.txt > arg.txt
 # on recupere toutes les contraintes (contiennent un &)
+echo "constraint extraction"
 sed '/&/!d' file.txt > contraintes.txt
 
 
@@ -13,6 +16,3 @@ sed -i "s/\= //g" contraintes.txt
 sed -i "s/^ *-//g" contraintes.txt
 sed -i "s/,//g" arg.txt
 rm file.txt
-
-# on appelle le fichier python
-python3 question1.py $1 arg.txt contraintes.txt commandes.txt
--- TP2/test_combinatoire.sh
+++ TP2/test_combinatoire.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 `./extractor.sh /home/stev/tp2-app.sh`
+
+# on appelle le fichier python
+echo "cover table generation (this may take some time)"
+python3 question1.py $1 arg.txt contraintes.txt commands.txt
+
+echo "Start testing"
+while read command; do
+    `$(command)`
+done < commands.txt
