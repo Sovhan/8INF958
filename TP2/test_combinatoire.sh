48d6b8eaf019d72c74c52cf733917ec36e00e1dc
Olivier SOLDANO <olivier_soldano@hotmail.fr>
2016-03-20 17:37:04 -0400

    question 1 over

--- TP2/test_combinatoire.sh
+++ TP2/test_combinatoire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-`./extractor.sh /home/stev/tp2-app.sh`
+`./extractor.sh $1`
 
 # on appelle le fichier python
 echo "cover table generation (this may take some time)"
@@ -8,5 +8,6 @@ python3 question1.py $1 arg.txt contraintes.txt commands.txt
 
 echo "Start testing"
 while read command; do
-    `$(command)`
+    echo "${command}"
+    `${command}`
 done < commands.txt
