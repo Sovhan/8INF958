5e7592855f833fdf29b5b1c9a75b9b7ead80ab4d
Soccent74 <bourgeaux.benj@gmail.com>
2016-03-20 23:00:59 -0400

    Ajout du test si libre office est ouvert, puis d'un click sur le bouton open

--- TP2/question2.py
+++ TP2/question2.py
@@ -1,5 +1,13 @@
 # coding : utf-8
 import xmlrpclib
+proxy = xmlrpclib.ServerProxy("http://localhost:4118")
 
-p = xmlrpclib.ServerProxy("http://localhost:4118")
-p.launchapp("libreoffice", ["--calc"])
+proxy.launchapp("libreoffice", ["--calc"])
+from ldtp import *
+#permet de chercher si la fenetre que je viens d'ouvrir existe.
+result = guiexist('Untitled 1 - Libre Office Calc')
+print(result)
+
+#Commande pour clicker sur le bouton open.
+click('*-LibreOffice Calc', 'btnOpen')
+
