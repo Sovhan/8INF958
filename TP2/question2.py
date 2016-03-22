eed2aa1fcca006a042f4c9162baea36f3c0e1136
Soccent74 <bourgeaux.benj@gmail.com>
2016-03-22 10:19:52 -0400

    Question2;py

--- TP2/question2.py
+++ TP2/question2.py
@@ -1,13 +1,20 @@
-# coding : utf-8
+#encoding : utf-8
 import xmlrpclib
-proxy = xmlrpclib.ServerProxy("http://localhost:4118")
-
-proxy.launchapp("libreoffice", ["--calc"])
 from ldtp import *
-#permet de chercher si la fenetre que je viens d'ouvrir existe.
-result = guiexist('Untitled 1 - Libre Office Calc')
-print(result)
 
+proxy = xmlrpclib.ServerProxy("http://localhost:4118")
+#proxy.launchapp("libreoffice", ['--calc'])
+
+#Nom des fenetre de travail.
+nameWindowsPrincipale = "Untitled 1 - Libre Office Calc"
+nameSpreadsheet = "Untitled1 - LibreOffice Spreadsheets"
+#permet de chercher si la fenetre que je viens d'ouvrir existe.
+
+waittillguiexist(nameWindowsPrincipale)
+result = guiexist(nameWindowsPrincipale)
+print(result)
+selectmenuitem('*-LibreOffice Calc', 'Undo')
 #Commande pour clicker sur le bouton open.
-click('*-LibreOffice Calc', 'btnOpen')
+#checkrow(nameFile, 'Sheet1', 1)
+
 
