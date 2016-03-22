2f8510481e14a2d9612b45ef23bc9732aa9dca47
Olivier SOLDANO <olivier_soldano@hotmail.fr>
2016-03-22 04:27:28 -0400

    typo in echoes

--- TP2/extractor.sh
+++ TP2/extractor.sh
@@ -1,34 +1,34 @@
 #netoyage des entrées
 if [ $# -ne 1 ]
 then
-	echo "one parameter expected, got ${#}"
-	echo "usage extractor.sh <app_path>"
+	echo -e "\e[33mone parameter expected, got ${#}"
+	echo -e "usage extractor.sh <app_path>\e[0m"
         exit 1 
 fi
 
 
 # on recupere la sortie du -h dans file.txt
-echo -e "\nreading application help"
+echo -e '\nreading application help'
 if [ -n ${1:?} ] 
 then
 	$1 -h | sed -e '/^$/d' > file.txt
-	echo -e "\e[1;92m OK \e[0m"
+	echo -e '\e[1;92m OK \e[0m'
 
 # on recupere tous les arguments (ligne commence par  -)
-	echo -e "arguments extraction"
+	echo -e 'arguments extraction'
 	sed '/^ *-/!d' file.txt > arg.txt
-	echo -e "\e[1;92m OK \e[0m"
+	echo -e '\e[1;92m OK \e[0m'
 # on recupere toutes les contraintes (contiennent un &)
-	echo -e "constraints extraction"
+	echo -e 'constraints extraction'
 	sed '/&/!d' file.txt > contraintes.txt
 	echo -e "\e[1;92m OK \e[0m"
 
 # on efface les &, = dans contraintes.txt et les virgules dans arg.txt
-	echo -e "arguments and constraints files formating"
+	echo -e 'arguments and constraints files formating'
 	sed -i "s/& //g" contraintes.txt
 	sed -i "s/\= //g" contraintes.txt
 	sed -i "s/^ *-//g" contraintes.txt
 	sed -i "s/,//g" arg.txt
 	rm file.txt
-	echo -e "\e[1;92m OK \e[0m\n"
+	echo -e '\e[1;92m OK \e[0m\n'
 fi
--- TP2/test_combinatoire.sh
+++ TP2/test_combinatoire.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
-eval "./extractor.sh $1"
-
-# on appelle le fichier python
-echo "cover table generation and checking (this may take some time)"
-python3 question1.py $1 arg.txt contraintes.txt commands.txt
-echo -e "\e[1;92m 0K \e[0m"
-echo -e "\e[1;4mStarting tests on $1\n\e[0m"
-while read command; do
-    `${command}`
-done < commands.txt
-echo -e "\e[1;92m 0K \e[0m"
-rm arg.txt contraintes.txt commands.txt
+if [ ! -z ${1} ]
+then
+	eval "./extractor.sh $1"
+	# on appelle le fichier python
+	echo "cover table generation and checking (this may take some time)"
+	python3 question1.py $1 arg.txt contraintes.txt commands.txt
+	echo -e "\e[1;92m 0K \e[0m"
+	echo -e "\e[1;4mStarting tests on $1\n\e[0m"
+	while read command; do
+	    `${command}`
+	done < commands.txt
+	echo -e "\e[1;92m 0K \e[0m"
+	rm arg.txt contraintes.txt commands.txt
+else
+	echo -e "\e[33m Usage : ./test_combinatoire.sh <app_path>\e[0m"
+fi
