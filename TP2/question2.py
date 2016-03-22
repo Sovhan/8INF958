031c7cc517d0d3598c7facf841e3996a21c5a1f5
Soccent74 <bourgeaux.benj@gmail.com>
2016-03-22 13:16:22 -0400

    Ajout dernire V. de question2.py

--- TP2/question2.py
+++ TP2/question2.py
@@ -1,19 +1,38 @@
 #encoding : utf-8
 import xmlrpclib
+from dogtail.procedural import focus
+
 from ldtp import *
 
 proxy = xmlrpclib.ServerProxy("http://localhost:4118")
-#proxy.launchapp("libreoffice", ['--calc'])
+
 
 #Nom des fenetre de travail.
 nameWindowsPrincipale = "Untitled 1 - Libre Office Calc"
-nameSpreadsheet = "Untitled1 - LibreOffice Spreadsheets"
 #permet de chercher si la fenetre que je viens d'ouvrir existe.
-
+proxy.launchapp("libreoffice", ['--calc'])
 waittillguiexist(nameWindowsPrincipale)
 result = guiexist(nameWindowsPrincipale)
 print(result)
-selectmenuitem('*-LibreOffice Calc', 'Undo')
+
+#proxy.generatemouseevent(400, 0)
+proxy.activatewindow(nameWindowsPrincipale)
+print("cense etre affiche")
+
+r = proxy.doesmenuitemexist(nameWindowsPrincipale, 'Tools')
+print(r)
+s = proxy.doesmenuitemexist(nameWindowsPrincipale, 'btnTools')
+print(s)
+#proxy.invokemenu(nameWindowsPrincipale, 'Tools')
+
+
+
+#Test des 4 possibilite.
+#proxy.selectmenuitem(nameWindowsPrincipale, 'mnuEdit')
+#proxy.click(nameWindowsPrincipale, 'mnuEdit')
+#proxy.selectmenuitem(nameWindowsPrincipale, 'Edit')
+#proxy.click(nameWindowsPrincipale, 'Edit')
+
 #Commande pour clicker sur le bouton open.
 #checkrow(nameFile, 'Sheet1', 1)
 
