75336cd4668f6e9bfd3979cf5777d8fd8cec625f
Soccent74 <bourgeaux.benj@gmail.com>
2016-03-22 15:35:49 -0400

    New

--- TP2/question2.py
+++ TP2/question2.py
@@ -1,28 +1,30 @@
 #encoding : utf-8
 import xmlrpclib
-from dogtail.procedural import focus
-
 from ldtp import *
 
 proxy = xmlrpclib.ServerProxy("http://localhost:4118")
 
 
 #Nom des fenetre de travail.
-nameWindowsPrincipale = "Untitled 1 - Libre Office Calc"
+nameWindowsPrincipale = "Untitled 1 - LibreOffice Calc"
 #permet de chercher si la fenetre que je viens d'ouvrir existe.
-proxy.launchapp("libreoffice", ['--calc'])
-waittillguiexist(nameWindowsPrincipale)
+#proxy.launchapp("libreoffice", ['--calc'])
+#waittillguiexist(nameWindowsPrincipale)
+
+print("Test existence de la fenetre.")
 result = guiexist(nameWindowsPrincipale)
 print(result)
 
 #proxy.generatemouseevent(400, 0)
-proxy.activatewindow(nameWindowsPrincipale)
-print("cense etre affiche")
-
-r = proxy.doesmenuitemexist(nameWindowsPrincipale, 'Tools')
+print("Test existence bouton.")
+#proxy.selectmenuitem('*Calc', 'mnuFile;mnuNew')
+r = proxy.doesmenuitemexist(nameWindowsPrincipale, 'mnuFile;mnuNew')
 print(r)
-s = proxy.doesmenuitemexist(nameWindowsPrincipale, 'btnTools')
-print(s)
+#s = proxy.doesmenuitemexist(nameWindowsPrincipale, 'Libre Office Calc btnTools;Solver...')
+#print(s)
+#t = proxy.doesmenuitemexist(nameWindowsPrincipale, 'Libre Office Calc mbarTools;Solver...')
+#print(t)
+
 #proxy.invokemenu(nameWindowsPrincipale, 'Tools')
 
 
