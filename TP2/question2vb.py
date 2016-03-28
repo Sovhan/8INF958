5e634c4dfd20af0d4ebbecddee80d87d108ea635
Soccent74 <bourgeaux.benj@gmail.com>
2016-03-28 16:55:43 -0400

    ajout last version

--- TP2/question2vb.py
+++ TP2/question2vb.py
@@ -6,29 +6,25 @@ proxy = xmlrpclib.ServerProxy("http://localhost:4118")
 
 
 # Nom des fenetre de travail.
-nameWindowsPrincipale = "Untitled 1 - docPourBug1.ods"
+nameWindowsPrincipale = "docPourBug1.ods - LibreOffice Calc"
 namePaneSecurity = "LibreOffice - Security Warning"
+
 # permet de chercher si la fenetre que je viens d'ouvrir existe.
 if not guiexist(nameWindowsPrincipale):
-    proxy.launchapp("libreoffice", ['-o', 'docPourBug1.ods'] )
+    proxy.launchapp("libreoffice", ['-o', 'Documents/8INF958/TP2/docPourBug1.ods'] )
     waittillguiexist(namePaneSecurity)
-    proxy.click(namePaneSecurity, 'Enable Macros')
+    try:
+        proxy.click(namePaneSecurity, 'Enable Macros')
+    except Exception:
+        proxy.generatekeyevent('<enter>')
+#waittillguiexist(nameWindowsPrincipale)
 
-waittillguiexist(nameWindowsPrincipale)
+
 
 print("Test existence de la fenetre.")
 result = guiexist(nameWindowsPrincipale)
 print(result)
 
 print("Test existence bouton.")
-r = proxy.doesmenuitemexist(nameWindowsPrincipale, 'mnuTools')
-
-print(r)
-pane = 'Macro Selector'
-if r:
-    proxy.selectmenuitem(nameWindowsPrincipale, 'mnuTools')
-    proxy.click(nameWindowsPrincipale, 'mnuRun Macro...')
-    proxy.closewindow(pane)
-    proxy.selectmenuitem(nameWindowsPrincipale, 'mnuTools')
-    proxy.click(nameWindowsPrincipale, 'mnuRun Macro...')
+r = proxy.click(nameWindowsPrincipale, 'btnmacroBug1')
 
