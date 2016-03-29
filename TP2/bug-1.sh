55d5d52bcaa4e2739416797a14e5c206c16863b5
basile fetet <basile.fetet@gmail.com>
2016-03-29 13:26:41 -0400

    ajout des echos d'explication pour le bug 1

--- TP2/bug-1.sh
+++ TP2/bug-1.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+echo "ceci est le programme pour tester le bug 1"
+echo "apres de nombreux tests nous avons reussi a tester sur libre office 3 et 5 via une VM que nous avons cree"
+echo "note importante : avant toute chose il faut avoir modifie le parametre de securite des macros :"
+echo "- ouvrir libre office"
+echo "-dans tools -> options -> security -> macro security -> modifier la securite a medium"
+echo "sur le serveur du cours il nous manque des composants afin de tester ce bug de maniere automatique :"
+echo "- at-spi"
+echo "nous avons aussi reussi a prouver que ce bug, qui normalement a ete corrige, n'a ete que contourne (cf explication en texte dans le document libre office docPourBug1.ods"
+ 
+
 libreoffice -o docPourBug1.ods &
 python question2vb.py &
