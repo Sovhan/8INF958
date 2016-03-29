e28b81c6fe3bbe8c9a623c493f73beea61910d67
Olivier SOLDANO <olivier_soldano@hotmail.fr>
2016-03-28 17:01:00 -0700

    update question2-2.py

--- TP2/question2-2.py
+++ TP2/question2-2.py
@@ -8,26 +8,29 @@ proxy = xmlrpclib.ServerProxy("http://localhost:4118")
 nameWindowsPrincipale = "Untitled 1 - LibreOffice Writer"
 # permet de chercher si la fenetre que je viens d'ouvrir existe.
 if not guiexist(nameWindowsPrincipale):
-    proxy.launchapp("libreoffice", ['--writer'])
+    proxy.launchapp("libreoffice4.3", ['--writer'])
 waittillguiexist(nameWindowsPrincipale)
 
 print("Test existence de la fenetre.")
 result = guiexist(nameWindowsPrincipale)
 print(result)
-
+if result:
+    proxy.activatewindow(nameWindowsPrincipale)
 for _ in range(10):
     proxy.generatekeyevent('f')
+    wait(0.05)
     proxy.generatekeyevent('<ctrl><enter>')
 
 proxy.generatekeyevent('<ctrl>f')
-wait(1)
+wait(0.3)
 proxy.generatekeyevent('f')
-print(proxy.getchild(nameWindowsPrincipale, 'Find Next'))
 proxy.mousemove(nameWindowsPrincipale, 'btnFindNext')
 proxy.mouseleftclick(nameWindowsPrincipale, 'btnFindNext')
-wait(1)
-proxy.generatekeyevent('<enter>')
-proxy.generatekeyevent('<ctrl>f')
-proxy.generatekeyevent('<ctrl>f')
-proxy.generatekeyevent('<ctrl>f')
 
+proxy.mousemove(nameWindowsPrincipale, 'btnFindNext')
+wait(1)
+proxy.generatekeyevent('<ctrl>f')
+proxy.keypress('<ctrl>')
+proxy.keypress('f')
+proxy.keyrelease('f')
+proxy.keyrelease('<ctrl>')
